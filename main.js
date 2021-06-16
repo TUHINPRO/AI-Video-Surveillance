b0c18336b733dd4a7edf96175a0648fa16acdd2f
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-06-16 15:57:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 video="";
 status="";
+objects=[];
+
 function preload() {
     video=createVideo("video.mp4");
     video.hide();
@@ -10,11 +12,7 @@ function setup(){
 
 }
 
-function draw() {
-    image(video,0,0,600,400);
 
-
-}
 
 function ModelLoaded() {
 video.loop();
@@ -29,3 +27,33 @@ anything=ml5.objectDetector('cocossd',ModelLoaded);
 document.getElementById("status").innerHTML="Status:Detecting Objects";
 
 }
+
+
+
+function gotResult(error,results) {
+    if(error) {
+        console.error(error);
+
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+    
+}
+function draw() {
+    image(video,0,0,600,400);
+if(status!=""){
+    anything.detect(video,gotResult);
+    for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML= "Status : Objects are detected";
+        document.getElementById("number_of_objects").innerHTML="Numbers of objects detected are"+objects.length;
+
+        fill("#FF0000");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}}
